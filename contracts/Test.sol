2a1b1324adb9a1559eaf4395644fcdb86411b2ee
Alan Jiang <adj2424@gmail.com>
2023-03-10 16:16:39 -0500

    refactor

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,13 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 contract Test {
-    struct Person {
-        string name;
-        address personAddress;
-        bool checkedIn;
-    }
-
     struct Event {
         uint32 id;
         string eventName;
@@ -15,7 +9,8 @@ contract Test {
         uint256 eventDate;
         uint32 capacity;
         uint32 deposit;
-        Person[] rsvp;
+        address[] rsvpAddress;
+        bool[] rsvpCheckIn;
     }
 
     uint32 eventID = 0;
@@ -31,57 +26,56 @@ contract Test {
         uint32 deposit
     ) external {
         // set to external because it cannot be accessed internally, only externally to save gass
-        Person[] memory rsvpList;
-        // initialize event into mapping
-        events[eventID].id = eventID;
-        // get initialized event and set its values
-        Event memory e = events[eventID];
-        e = Event({
+
+        // create event
+        Event memory e = Event({
             id: eventID,
             eventName: eventName,
             eventCreator: eventCreator,
             eventDate: eventDate,
             capacity: capacity,
             deposit: deposit,
-            rsvp: rsvpList
+            rsvpAddress: new address[](0),
+            rsvpCheckIn: new bool[](0)
         });
+        events[eventID] = e;
         eventID++;
     }
 
     // rsvp to an event by paying a small deposit
     function rsvp(string memory name, uint32 id) external payable {
-        require(events[id].rsvp.length < events[id].capacity, "Event is full");
+        require(
+            events[eventID].rsvpAddress.length < events[id].capacity,
+            "Event is full"
+        );
         require(events[id].eventDate > block.timestamp, "Event has passed");
         // enough ether sent
         require(msg.value == events[id].deposit, "Deposit is incorrect");
-        Person memory person = Person({
-            name: name,
-            personAddress: msg.sender,
-            checkedIn: false
-        });
-        events[id].rsvp.push(person);
+
+        events[eventID].rsvpAddress.push(msg.sender);
+        events[eventID].rsvpCheckIn.push(false);
     }
 
     // check in to an event and receive the deposit back
-    function checkIn(address personAddress, uint32 id) public {
+    function checkIn(address personAddress, uint32 id) external {
         require(
             events[id].eventDate < block.timestamp,
             "Event has not started"
         );
-        for (uint32 i = 0; i < events[id].rsvp.length; i++) {
+        for (uint32 i = 0; i < events[eventID].rsvpAddress.length; i++) {
             // check if person is in the rsvp list and has not checked in
             if (
-                events[id].rsvp[i].personAddress == personAddress &&
-                !events[id].rsvp[i].checkedIn
+                events[id].rsvpAddress[i] == personAddress &&
+                !events[id].rsvpCheckIn[i]
             ) {
-                events[id].rsvp[i].checkedIn = true;
+                events[id].rsvpCheckIn[i] = true;
                 // transfer deposit from contract to person
                 payable(personAddress).transfer(events[id].deposit);
             }
         }
     }
 
-    function withdrawUnclaimed(uint32 id) public {
+    function withdrawUnclaimed(uint32 id) external {
         require(
             block.timestamp > events[id].eventDate,
             "Withdraw after event!"
@@ -90,12 +84,20 @@ contract Test {
             events[id].eventCreator == msg.sender,
             "Only creator can withdraw"
         );
-        for (uint256 i = 0; i < events[id].rsvp.length; i++) {
+        for (uint32 i = 0; i < events[eventID].rsvpAddress.length; i++) {
             // cannot be checked in
-            if (!events[id].rsvp[i].checkedIn) {
+            if (!events[id].rsvpCheckIn[i]) {
                 // transfer deposit from contract to event owner
                 payable(events[id].eventCreator).transfer(events[id].deposit);
             }
         }
+    }
+
+    function getEvent(uint32 id) public view returns (Event memory) {
+        return events[id];
+    }
+
+    function getEventsSize() public view returns (uint32) {
+        return eventID;
     }
 }
