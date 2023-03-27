f0d259156e9dbd97d83fddf7cfe3eda12ae10d09
Alan Jiang <adj2424@gmail.com>
2023-03-27 15:01:26 -0400

    idk anymore

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,36 +1,37 @@
-import './polyfills';
+import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import type { AppProps } from 'next/app';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
+import { mainnet, polygon, polygonMumbai } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const { chains, provider } = configureChains(
+const { chains, provider, webSocketProvider } = configureChains(
 	[mainnet, polygon, polygonMumbai],
-	[alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+	[(alchemyProvider({ apiKey: process.env.ALCHEMY_ID! }), publicProvider())]
 );
 
 const { connectors } = getDefaultWallets({
-	appName: 'RainbowKit demo',
+	appName: 'RainbowKit App',
 	chains
 });
 
 const wagmiClient = createClient({
 	autoConnect: true,
 	connectors,
-	provider
+	provider,
+	webSocketProvider
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
 		<WagmiConfig client={wagmiClient}>
 			<RainbowKitProvider chains={chains}>
-				<App />
+				<Component {...pageProps} />
 			</RainbowKitProvider>
 		</WagmiConfig>
-	</React.StrictMode>
-);
+	);
+}
+
+export default MyApp;
